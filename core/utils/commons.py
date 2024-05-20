129cb8a9165c22b3cb01f547391e323a85c57d09
DESKTOP-8TGIBBQ\zhanghe <123456@qq.com>
2024-05-20 23:36:53 +0800

    处理bug

--- core/utils/commons.py
+++ core/utils/commons.py
@@ -34,14 +34,10 @@ def login_required(view_func):
 
     return wrapper
 
-
-
-
-# @login_required
-# def set_user_avatar():
-#     # user_id = session.get("user_id")
-#     user_id = g.user_id
-#     return json  ""
-#
-#
-# set_user_avatar()  -> wrapper()
+def deep_merge_dicts(dict1, dict2):
+    for key in dict2:
+        if key in dict1 and isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
+            deep_merge_dicts(dict1[key], dict2[key])
+        else:
+            dict1[key] = dict2[key]
+    return dict1
--- crawling/stock_hold_management_detail_cninfo.py
+++ crawling/stock_hold_management_detail_cninfo.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+import os.path
+import sys
+from flask import Flask
+cpath_current = os.path.dirname(os.path.dirname(__file__))
+cpath = os.path.abspath(os.path.join(cpath_current))
+print(cpath)
+sys.path.append(cpath)
 import datetime
 import pandas as pd
 import requests
-
+from core.utils.commons import deep_merge_dicts
 
 
 def stock_hold_management_detail_cninfo(symbol: str = "增持") -> pd.DataFrame:
@@ -94,5 +102,83 @@ def stock_hold_management_detail_cninfo(symbol: str = "增持") -> pd.DataFrame:
     temp_df["期末市值"] = pd.to_numeric(temp_df["期末市值"], errors="coerce")
     return temp_df
 
+# 东财数据中心基础数据
+def stock_detail_em(dict2) -> pd.DataFrame:
+    """
+    东方财富网-数据中心-资金流向-排名
+    https://data.eastmoney.com/executive/gdzjc.html
+    """
+    url = "https://datacenter-web.eastmoney.com/api/data/v1/get"
+    dict1 = {
+        "pageSize":6000,
+        "pageNumber":1,
+        'quoteType':0,
+        'source':'WEB',
+        "client":'WEB',
+       
+    }
+    params = deep_merge_dicts(dict1, dict2)
+    r = requests.get(url, params=params)
+    data_json = r.json()
+    print(data_json)
+    temp_df = pd.DataFrame(data_json["result"]["data"])
+    print(temp_df)
+    return temp_df
+
+# 股东户数（最新）
+def stock_management_detail() -> pd.DataFrame:
+    """
+    """
+    params = {
+        "sortColumns":"HOLDER_NUM",
+        "sortTypes":"1",
+        "reportName":"RPT_HOLDERNUMLATEST",
+        "columns":"SECURITY_CODE,SECURITY_NAME_ABBR,END_DATE,INTERVAL_CHRATE,HOLDER_NUM,PRE_HOLDER_NUM,HOLDER_NUM_CHANGE,HOLDER_NUM_RATIO,END_DATE,PRE_END_DATE",
+    }
+    temp_df= stock_detail_em(params)
+    temp_df.rename(
+        columns={
+            "SECURITY_CODE": "代码",
+            "SECURITY_NAME_ABBR": "名称",
+            "f2": "最新价",
+            "f3": "涨跌幅",
+            "HOLDER_NUM": "本次股东户数",
+            "PRE_HOLDER_NUM": "上次股东户数",
+            "HOLDER_NUM_CHANGE": "增减",
+            "HOLDER_NUM_RATIO": "增减比例",
+            "INTERVAL_CHRATE": "区间涨跌幅",
+            "END_DATE": "本期报告",
+            "PRE_END_DATE": "上期报告",
+        },
+        inplace=True,
+    )
+    print(temp_df)
+    return temp_df
+
+# 股东增减持
+def stock_management_increase_detail_em() -> pd.DataFrame:
+    params = {
+       "sortColumns":"END_DATE,SECURITY_CODE,EITIME",
+        "sortTypes":"-1,-1,-1",
+        "reportName":"RPT_SHARE_HOLDER_INCREASE",
+        "columns":"f2,SECURITY_CODE,SECURITY_NAME_ABBR,NEWEST_PRICE,CHANGE_RATE_QUOTES,CHANGE_NUM,AFTER_CHANGE_RATE,CHANGE_FREE_RATIO",
+        'quoteColumns':'f2~01~SECURITY_CODE~NEWEST_PRICE,f3~01~SECURITY_CODE~CHANGE_RATE_QUOTES',
+        'filter':f'(DIRECTION="增持")'
+    }
+    temp_df= stock_detail_em(params)
+    temp_df.rename(
+        columns={
+            "SECURITY_CODE": "代码",
+            "SECURITY_NAME_ABBR": "名称",
+            "NEWEST_PRICE": "最新价",
+            "CHANGE_RATE_QUOTES": "涨跌幅",
+            "CHANGE_NUM": "变动数量(万股)",
+            "AFTER_CHANGE_RATE": "占总股本比例",
+            "CHANGE_FREE_RATIO": "占流通股比例",
+        },
+        inplace=True,
+    )
+    print(temp_df)
+    return temp_df
 if __name__ == '__main__':
-    stock_hold_management_detail_cninfo()
+    stock_management_detail()
