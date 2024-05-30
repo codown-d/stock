fa460cfb0606ae38a1a3aa86044eb6c634413f69
DESKTOP-SQL335Q\zhanghe <123456@qq.com>
2024-05-30 18:34:26 +0800

    no message

--- job/fetch_stock_basic_data.py
+++ job/fetch_stock_basic_data.py
@@ -15,27 +15,22 @@ cpath = os.path.abspath(os.path.join(cpath_current))
 sys.path.append(cpath)
 from core.utils.commons import gp_type_szsh
 import akshare as ak
-
-ret_values = []
 def fetch_stocks_all_code():
     try:
         stock_info_a_code_name_df = ak.stock_info_a_code_name()
         result = stock_info_a_code_name_df.to_dict(orient='records') 
         with pd.ExcelWriter(f'{cpath}/stock_date/stock.xlsx') as writer:
-        # if(1):
             with ThreadPoolExecutor(max_workers=1000) as executor:
                 to_do = []
                 for res in result:
                     code = res['code']
-                    if(gp_type_szsh(code)==True and int(code)<3):
+                    if(gp_type_szsh(code)==True and int(code)<1000):
                         future = executor.submit(fetch_stocks, code)
-                        to_do.append({'future':future,'code':code})
-                for d in as_completed(to_do):  # 并发执行
+                        to_do.append(future)
+                for future in as_completed(to_do):  # 并发执行
+                    d = future.result()
                     code = d['code']
-                    future = d['future']
-                    date = future.result()
-                    print(date)
-                    print(code)
+                    date = d['date']
                     date.to_excel(writer, sheet_name=code,index=False)
     except Exception as e:
         logging.error(f"fetch_stocks处理异常：{e}")
@@ -57,7 +52,8 @@ time = arrow.now().format("YYYYMMDD")
 def fetch_stocks(code):
     try:
         stock_zh_a_hist_df = ak.stock_zh_a_hist(symbol=code, period="daily", start_date="20200101", end_date=time, adjust="")
-        return stock_zh_a_hist_df
+        print(code)
+        return {'date':stock_zh_a_hist_df,'code':code}
     except Exception as e:
         logging.error(f"fetch_stocks处理异常：{e}")
     return None
