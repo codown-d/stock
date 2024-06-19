177158f0697aaf1d14db3e53a329cdfd63bf48ae
DESKTOP-SQL335Q\zhanghe <123456@qq.com>
2024-06-19 18:19:58 +0800

    no message

--- job/get_stock_volume.py
+++ job/get_stock_volume.py
@@ -78,7 +78,7 @@ def run(f, my_iter):
 def fetch_stocks(code,time=arrow.now().format("YYYY-MM-DD")):
     
     err_coor_path=f'{cpath}/stock_date/{time}_error_code.csv'
-    time='2024-06-12'
+    time='2024-06-17'
     try:
         temp_df = dfcf.stock_history_fenshi_detail(code,f"{time} 09:25:00", f"{time} 15:00:00")
         return {'data':temp_df,'code':code}
@@ -113,7 +113,6 @@ def handle_error_tick_volume():
 def handle_vol():
     start_date = datetime(2024, 6, 6,9, 30, 00)
     end_date = datetime(2024,6, 6,10, 30, 00)
-    print(StockTimePrice.时间.between(start_date, end_date),end_date)
     stock = StockTimePrice.query.filter(StockTimePrice.时间.between(start_date, end_date)).all()
     df = pd.DataFrame([(r.ID, r.时间, r.代码, r.开盘,r.收盘,r.最高,r.最低,r.成交量,r.成交额,r.均价) for r in stock])
     df.columns=['ID', '时间', '代码', '开盘','收盘','最高','最低','成交量','成交额','均价']
---
+++ job/save_stock_vol_orc.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from datetime import datetime
+import logging
+import os.path
+import sys
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import zoneinfo
+import pyorc
+
+cpath_current = os.path.dirname(os.path.dirname(__file__))
+cpath = os.path.abspath(os.path.join(cpath_current))
+print(cpath)
+sys.path.append(cpath)
+
+from core.models import StockTimePrice
+
+path =  f'{cpath}/output.orc'
+def save_stocks_vol():
+    start_date = datetime(2024, 6, 6,9, 30, 00)
+    end_date = datetime(2024,6, 6,15, 00, 00)
+    try:
+        stock = StockTimePrice.query.filter(StockTimePrice.时间.between(start_date, end_date)).all()
+        df = pd.DataFrame([(r.ID, r.时间, r.代码, r.开盘,r.收盘,r.最高,r.最低,r.成交量,r.成交额,r.均价) for r in stock])
+        df.columns=['ID', '时间', '代码', '开盘','收盘','最高','最低','成交量','成交额','均价']
+        print(df)
+        df.to_orc(path)
+        return df
+    except Exception as e:
+        logging.error(f"fetch_stocks处理异常：{e}")
+    return None
+def read_stocks_vol():
+    try:
+        df = pd.read_orc(path)
+        print(df)
+        return True
+    except Exception as e:
+        logging.error(f"fetch_stocks处理异常：{e}")
+    return None
+if __name__ == '__main__':
+    read_stocks_vol()
